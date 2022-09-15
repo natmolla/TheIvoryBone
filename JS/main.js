b4fd53b5d39278c12a01a91f3b382ad6ef6977d4
Natalia <natalia.molla222@gmail.com>
2022-09-15 15:36:34 -0700

    added login

--- JS/main.js
+++ JS/main.js
@@ -10,3 +10,39 @@ menu.addEventListener('click', () => {
 close.addEventListener('click', () => {
     nav.classList.remove('open-nav')
 })
+
+
+let loginbutton = document.getElementById('loggingin');
+let usernameInput = document.getElementById('username');
+let passwordInput = document.getElementById('password');
+let wrongloginbox = document.getElementById('wronglogin');
+let closebutton = document.getElementById('closewindow');
+
+loginbutton.addEventListener("click", tryLoggingIn);
+loginbutton.addEventListener("click", clearFields);
+
+
+async function tryLoggingIn() {
+ let username = usernameInput.value;
+ let password = passwordInput.value;
+
+ let response = await fetch("http://localhost:9000/monstermanual/player/"+ username +"/"+password);
+ response = await response.json();
+ console.log(response);
+
+    if (response == true) {
+        console.log(response);
+        window.location = "playerprofile.html";
+        document.cookie = "playerName=" + username;
+    }
+
+    else if(response == false) {
+        wrongloginbox.innerHTML = "";
+        wrongloginbox.innerHTML = "Wrong login credentials, try again."
+    }
+
+}
+
+function clearFields() {
+    wrongloginbox.innerHTML = "";
+}
